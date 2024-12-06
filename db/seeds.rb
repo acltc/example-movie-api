4eebf8408de6d8cdeea0fdcbd87f4f13c6a91ddf
Peter Jang <peter.x.jang@gmail.com>
2024-12-05 22:18:08 -0600

    Add actor model with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Actor.create(first_name: "Regina", last_name: "King", known_for: "Watchmen")
+Actor.create(first_name: "Nicolas", last_name: "Cage", known_for: "The Unbearable Weight of Massive Talent")
+Actor.create(first_name: "Frances", last_name: "McDormand", known_for: "Almost Famous")
+Actor.create(first_name: "Jeff", last_name: "Bridges", known_for: "Tron: Legacy")
+Actor.create(first_name: "Vera", last_name: "Farmiga", known_for: "Up in the Air")
