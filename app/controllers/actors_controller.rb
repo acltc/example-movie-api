9981d7c52e26bcfa51e2bac3c917cb9addab7e21
Peter Jang <peter.x.jang@gmail.com>
2024-12-05 22:31:32 -0600

    Add create action

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,13 @@ class ActorsController < ApplicationController
     @actors = Actor.all
     render :index
   end
+
+  def create
+    @actor = Actor.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+    )
+    render :show
+  end
 end
--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -8,4 +8,11 @@ class ActorsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Actor.count, data.length
   end
+
+  test "create" do
+    assert_difference "Actor.count", 1 do
+      post "/actors.json", params: { first_name: "Test", last_name: "Test", known_for: "Test" }
+      assert_response 200
+    end
+  end
 end
